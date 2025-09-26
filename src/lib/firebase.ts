230ae0b2cbf95e144572263313d76d69185d4d1c
Jefferson Nascimento <jeklindomotog@gmail.com>
2025-09-26 23:52:14 +0000

    corigir FirebaseError: Firebase: Error (auth/configuration-not-found).

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDWMZVt5HN-fWfcem_33cukV3xXsQBlzkk",
   authDomain: "studio-9537614342-74580.firebaseapp.com",
   projectId: "studio-9537614342-74580",
-  storageBucket: "studio-9537614342-74580.firebasestorage.app",
+  storageBucket: "studio-9537614342-74580.appspot.com",
   messagingSenderId: "903389706855",
   appId: "1:903389706855:web:44b4024e18b9fc5302f2c2"
 };
