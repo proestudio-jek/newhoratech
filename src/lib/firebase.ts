5f38eb953429389aaaafdc5701f465fb193629e4
Jefferson Nascimento <jeklindomotog@gmail.com>
2025-09-26 23:48:48 +0000

    adicionar o sdk do firebase // Import the functions you need from the SD

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,9 +6,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyDWMZVt5HN-fWfcem_33cukV3xXsQBlzkk",
   authDomain: "studio-9537614342-74580.firebaseapp.com",
   projectId: "studio-9537614342-74580",
-  storageBucket: "studio-9537614342-74580.appspot.com",
+  storageBucket: "studio-9537614342-74580.firebasestorage.app",
   messagingSenderId: "903389706855",
-  appId: "1:903389706855:web:1a3e34aa1e898ff402f2c2",
+  appId: "1:903389706855:web:44b4024e18b9fc5302f2c2"
 };
 
 // Initialize Firebase
