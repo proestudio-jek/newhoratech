f8cdd4f0a4e28ef6f4144d79207af2215fa06ba6
Jefferson Nascimento <jeklindomotog@gmail.com>
2025-10-19 23:41:53 +0000

    Try fixing this error: `Console Error: Firebase Auth Error (auth/configu

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAuth, type Auth } from "firebase/auth";
+import { getFirestore, type Firestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDWMZVt5HN-fWfcem_33cukV3xXsQBlzkk",
@@ -12,10 +12,19 @@ const firebaseConfig = {
   appId: "1:903389706855:web:44b4024e18b9fc5302f2c2"
 };
 
-// Initialize Firebase
-const app: FirebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
+let app: FirebaseApp;
+let auth: Auth;
+let db: Firestore;
 
-const auth = getAuth(app);
-const db = getFirestore(app);
+if (typeof window !== "undefined" && !getApps().length) {
+  app = initializeApp(firebaseConfig);
+  auth = getAuth(app);
+  db = getFirestore(app);
+} else if (getApps().length) {
+  app = getApp();
+  auth = getAuth(app);
+  db = getFirestore(app);
+}
 
-export { app, db, auth };
+// @ts-ignore
+export { app, auth, db };
