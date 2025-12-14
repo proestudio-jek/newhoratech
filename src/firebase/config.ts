1f246bb04998d5c36e0356c1464015480ca8923e
Jefferson Nascimento <jeklindomotog@gmail.com>
2025-12-14 18:01:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9537614342-74580",
+  "appId": "1:903389706855:web:1a3e34aa1e898ff402f2c2",
+  "apiKey": "AIzaSyDWMZVt5HN-fWfcem_33cukV3xXsQBlzkk",
+  "authDomain": "studio-9537614342-74580.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "903389706855"
+};
